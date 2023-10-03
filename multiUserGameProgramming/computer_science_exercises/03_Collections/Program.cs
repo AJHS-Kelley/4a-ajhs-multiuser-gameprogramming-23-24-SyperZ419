42bd6a2013001420171eeb091082688163c04e54
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-03 14:26:00 -0400

    v0.1a
    Declared and defined, string, integer, and float arrays.

--- multiUserGameProgramming/computer_science_exercises/03_Collections/Program.cs
+++ multiUserGameProgramming/computer_science_exercises/03_Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿// 03_Collections, Xavier Oliver, v0.0
+﻿// 03_Collections, Xavier Oliver, v0.1a
 using System;
 
 namespace _03_Collections
@@ -7,7 +7,23 @@ namespace _03_Collections
     {
         static void Main(string[] args)
         {
-            
+            // Collections are variables that can store multiple values in one variable.
+
+            /* Arrays
+            -- Number of elements in an array CANNOT change
+            -- Contents of elements in an array CAN change
+            -- Items in the array are called ELEMENTS
+            -- Arrays are ordered, meaning each item has a fixed position
+            -- The position in the array is known as the INDEX
+            -- FIrst element in an array is index 0
+            */
+
+            // Declaring and Defining an Array
+            string[] breakfastFoods = {"Bacon", "Waffles", "Pancakes", "Cereal", "Parfait"};
+            int[] testScores {99, 100, 25, 15, 27, 35};
+            float[] GPA = {3.14f, 2.25f, 1.74f, 1.99f, 0.99f, 4.25f};
+
+
         }
     }
 }
