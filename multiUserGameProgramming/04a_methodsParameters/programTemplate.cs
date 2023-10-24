d83bb1cb46c90015d88480c60a54040a5d965e08
SyperZ419 <s11328481@students.duvalschools.org>
2023-10-24 14:08:58 -0400

    Create programTemplate.cs
    Added MyMethod

---
+++ multiUserGameProgramming/04a_methodsParameters/programTemplate.cs
@@ -0,0 +1,25 @@
+// Xavier Oliver, MethodsParameters, v0.1
+using System;
+
+namespace MethodsParameters
+{
+    class MethodsParameters
+    {
+        // METHOD --  named block of code, can be used over and over
+        // All methods have a SIGNATURE that defines their name, parameters, and output
+        // Example Signature
+        static void MyMethod()
+        {
+            Console.WriteLine("I like mine with waffles, chicken, and eggs, with a side of hash-browns\n");
+        }
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+
+    }
+
+
+
+
+}
