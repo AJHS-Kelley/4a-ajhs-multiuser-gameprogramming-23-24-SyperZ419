958e8c2d9f50e1e1912ba55669552b1e701b4fd1
SyperZ419 <s11328481@students.duvalschools.org>
2023-11-14 14:52:12 -0500

    Update exampleGameMethods.cs

--- multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
+++ multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
@@ -1,9 +1,38 @@
-// Xavier Oliver, Template, v0.0
+// Xavier Oliver, Template, v0.1
 using System;
 
-namespace Updateforeachprogram
+namespace ExampleGameMethods
 {
-    class updateforeachprogram
+    class ExampleGameMethods
+    {
+    static string ChooseEnemy()
+    {
+        string[enemyList] = {"Goblin", "Goblin Chief", "Orc", "Skeleton", "Dragon", "Slime", "Yamato-no-Orochi", "Hydra", "Dark Warlock"} // List of Potential Enemies
+        string enemy = enemyList[rndNum.Next(0, enemyList.Length - 1)];
+        Console.WriteLine("You have encountered " + enemy + " !");
+        return enemy
+    }
+
+    static void MethodTwo()
+    {
+        pass
+    }
+
+    static void MethodThree()
+    {
+        pass
+    }
+
+    static void MethodFour()
+    {
+        pass
+    }
+
+
+
+
+
+
     {
         static void Main(string[] args)
         {
@@ -16,3 +45,4 @@ namespace Updateforeachprogram
 
 
 }
+    }
