71835f8df353ecdff56027d26e26a850707d5e51
SyperZ419 <s11328481@students.duvalschools.org>
2023-11-15 10:06:03 -0500

    Update exampleGameMethods.cs
    added semi-colons

--- multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
+++ multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
@@ -7,25 +7,25 @@ namespace ExampleGameMethods
     {
     static string ChooseEnemy()
     {
-        string[enemyList] = {"Goblin", "Goblin Chief", "Orc", "Skeleton", "Dragon", "Slime", "Yamato-no-Orochi", "Hydra", "Dark Warlock"} // List of Potential Enemies
+        string[enemyList] = {"Goblin", "Goblin Chief", "Orc", "Skeleton", "Dragon", "Slime", "Yamato-no-Orochi", "Hydra", "Dark Warlock"}; // List of Potential Enemies
         string enemy = enemyList[rndNum.Next(0, enemyList.Length - 1)];
         Console.WriteLine("You have encountered " + enemy + " !");
-        return enemy
+        return enemy;
     }
 
     static void MethodTwo()
     {
-        pass
+        pass;
     }
 
     static void MethodThree()
     {
-        pass
+        pass;
     }
 
     static void MethodFour()
     {
-        pass
+        pass;
     }
 
 
