495fac37672d54c94385e07534da16fe75c2ac38
SyperZ419 <s11328481@students.duvalschools.org>
2023-11-16 13:55:50 -0500

    Update exampleGameMethods.cs
    Finished ChooseEnemy method

--- multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
+++ multiUserGameProgramming/05a_exampleGameMethods/exampleGameMethods.cs
@@ -1,48 +1,49 @@
-// Xavier Oliver, Template, v0.1
+// Xavier Oliver, Template, v0.2
 using System;
 
 namespace ExampleGameMethods
 {
     class ExampleGameMethods
     {
-    static string ChooseEnemy()
-    {
-        string[enemyList] = {"Goblin", "Goblin Chief", "Orc", "Skeleton", "Dragon", "Slime", "Yamato-no-Orochi", "Hydra", "Dark Warlock"}; // List of Potential Enemies
-        string enemy = enemyList[rndNum.Next(0, enemyList.Length - 1)];
-        Console.WriteLine("You have encountered " + enemy + " !");
-        return enemy;
-    }
-
-    static void MethodTwo()
-    {
-        pass;
-    }
-
-    static void MethodThree()
-    {
-        pass;
-    }
-
-    static void MethodFour()
-    {
-        pass;
-    }
-
-
-
-
-
-
-    {
-        static void Main(string[] args)
+        static string ChooseEnemy()
         {
-            
+            Random rndNum = new Random();
+            string[] enemyList = {"Goblin", "Goblin Chief", "Orc", "Skeleton", "Dragon", "Slime", "Yamato-no-Orochi", "Hydra", "Dark Warlock"}; // List of Potential Enemies
+            string enemy = enemyList[rndNum.Next(0, enemyList.Length - 1)];
+            Console.WriteLine("You have encountered " + enemy + " !");
+            return enemy;
         }
 
+        /*static void MethodTwo()
+        {
+            pass;
+        }
+
+        static void MethodThree()
+        {
+            pass;
+        }
+
+        static void MethodFour()
+        {
+            pass;
+        }*/
+
+
+
+
+
+
+        
+        static void Main(string[] args)
+        {
+            ChooseEnemy();
+        }
+
+        
+
+
+
+
     }
-
-
-
-
 }
-    }
