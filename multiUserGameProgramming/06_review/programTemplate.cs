1211e84d83bfb521b034f5df728119fb0e6bff63
SyperZ419 <s11328481@students.duvalschools.org>
2024-01-10 14:05:51 -0500

    Create programTemplate.cs

---
+++ multiUserGameProgramming/06_review/programTemplate.cs
@@ -0,0 +1,49 @@
+// Xavier Oliver, Template, v0.0
+using System;
+
+namespace ReviewProject
+{
+    class ReviewProject
+    {
+        static void Main(string[] args)
+        {
+
+            // Declaring and Assigning Variables
+            // Integer -- -2,147,483,648 to 2,147,483,647
+            // Integer 4 bytes
+            int smtGames = 6;
+
+            // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            // Long -- 8 bytes
+            long length = 8349829829489239;
+
+            // Boolean -- true/false
+            // Boolean = 1 bit
+            bool correct = false;
+
+            // String -- 
+            // 2 bytes PER CHARACTER
+            // Must be in DOUBLE QUOTES " "
+            string food = "potato";
+
+            // Float -- Stores up to 6-7 decimal digits
+            // Float -- 4 bytes
+            float percent = 23.78F;
+
+            // Double -- Stores up to 15 decimal digits
+            // Double -- 8 bytes
+            double myDouble = 10.83467347D;
+
+            // Character (char)
+            // Single character, in SINGLE QUOTES ' '
+            // 2 bytes
+            char myChar = 'C';
+            
+        }
+
+    }
+
+
+
+
+}
