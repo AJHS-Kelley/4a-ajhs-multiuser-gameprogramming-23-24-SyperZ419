9c0e0fffd4e5a8cf7799ee9da1bb4568b2a26797
SyperZ419 <s11328481@students.duvalschools.org>
2024-01-10 14:35:11 -0500

    Update programTemplate.cs

--- multiUserGameProgramming/06_review/programTemplate.cs
+++ multiUserGameProgramming/06_review/programTemplate.cs
@@ -1,4 +1,4 @@
-// Xavier Oliver, Template, v0.0
+// Xavier Oliver, Review, v0.2
 using System;
 
 namespace ReviewProject
@@ -11,7 +11,7 @@ namespace ReviewProject
             // Declaring and Assigning Variables
             // Integer -- -2,147,483,648 to 2,147,483,647
             // Integer 4 bytes
-            int smtGames = 6;
+            //int smtGames = 6;
 
             // Long -- -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
             // Long -- 8 bytes
@@ -32,12 +32,38 @@ namespace ReviewProject
 
             // Double -- Stores up to 15 decimal digits
             // Double -- 8 bytes
-            double myDouble = 10.83467347D;
+            //double myDouble = 10.83467347D;
 
             // Character (char)
             // Single character, in SINGLE QUOTES ' '
             // 2 bytes
             char myChar = 'C';
+
+            // Type Casting -- changing one data type to another
+            // IMPLICIT -- happens automatically when going smaller -> larger
+            // char -> int -> long -> float -> double
+            
+            int myInt = 9;
+            double myDouble = myInt;
+            Console.WriteLine(myDouble);
+            
+            // EXPLICIT -- Must be done manually when going larger -> smaller
+            // double -> float -> long -> int -> char
+            double exampleDouble = 9.945738349D;
+            int myInt = (int) myDouble;
+
+            // Conversion Methods
+            Convert.ToString(); //String
+            Convert.ToDouble(); //Double
+            Convert.ToInt32(); //Integer
+            Convert.ToInt64(); //Long
+
+            Convert.ToString(myChar);
+            Convert.ToDouble(percent);
+            Convert.ToInt32(length);
+            Convert.ToInt64(exampleDouble);
+
+
             
         }
 
