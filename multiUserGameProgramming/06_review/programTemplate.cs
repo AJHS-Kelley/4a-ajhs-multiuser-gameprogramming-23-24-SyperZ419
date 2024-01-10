11c333f8474e9099045350cb6c82a3364f519850
SyperZ419 <s11328481@students.duvalschools.org>
2024-01-10 14:46:53 -0500

    Update programTemplate.cs

--- multiUserGameProgramming/06_review/programTemplate.cs
+++ multiUserGameProgramming/06_review/programTemplate.cs
@@ -58,10 +58,41 @@ namespace ReviewProject
             Convert.ToInt32(); //Integer
             Convert.ToInt64(); //Long
 
-            Convert.ToString(myChar);
-            Convert.ToDouble(percent);
-            Convert.ToInt32(length);
-            Convert.ToInt64(exampleDouble);
+            //Convert.ToString(myChar);
+            //Convert.ToDouble(percent);
+            //Convert.ToInt32(length);
+            //Convert.ToInt64(exampleDouble);
+
+            // Switch Statement
+            int dayOfWeek = 6;
+            switch (dayOfWeek)
+            {
+                case 1:
+                    Console.WriteLine("The day is Sunday.\n");
+                    break;
+                case 2:
+                    Console.WriteLine("The day is Monday.\n");
+                    break;
+                case 3:
+                    Console.WriteLine("The day is Tuesday.\n");
+                    break;
+                case 4:
+                    Console.WriteLine("The day is Wednesday.\n");
+                    break;
+                case 5:
+                    Console.WriteLine("The day is Thursday.\n");
+                    break;
+                case 6:
+                    Console.WriteLine("The day is Friday.\n");
+                    break;
+                case 7:
+                    Console.WriteLine("The day is Saturday.\n");
+                    break;
+                default:
+                    Console.WriteLine("No match found.\n");
+                    break; 
+
+            }
 
 
             
